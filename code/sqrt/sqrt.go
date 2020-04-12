5a060a5a42b41eb3fcf3a321d5f5efcd35f0f1fc
Souradeepta Biswas <sdb.svnit@gmail.com>
2020-04-12 17:11:41 -0500

    square root using methods

---
+++ code/sqrt/sqrt.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func main() {
+	result, err := sqrt(16)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Undefined for negative numbers")
+
+	}
+
+	return math.Sqrt(x), nil
+}
