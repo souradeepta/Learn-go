64a1745b09c25c87b6bc3812196a9a21bd09e1b3
Souradeepta Biswas <sdb.svnit@gmail.com>
2020-04-12 16:03:00 -0500

    calculator which only adds

---
+++ code/calculator/calculator.go
@@ -0,0 +1,19 @@
+// Create a calculator
+// short hand init declarations
+// x:= 5
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var x, y int
+	fmt.Print("Enter two numbers: ")
+	fmt.Scan(&x)
+	fmt.Scan(&y)
+
+	var sum int = x + y
+
+	fmt.Println("Sum is: ", sum)
+}
